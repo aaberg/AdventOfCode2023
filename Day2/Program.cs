a5240f61ba87fb5694c07cd2ce92d44cc9262e47
Lars Aaberg <lars@aaberg.cc>
2023-12-02 19:29:22 +0100

    Applied better formatting to Day 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -3,31 +3,37 @@
 using System.Reflection;
 using TheLibrary.Day2;
 
-var inputStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Day2.input.txt");
+
+// Read file and parse
+var inputStream = Assembly
+    .GetExecutingAssembly()
+    .GetManifestResourceStream("Day2.input.txt");
 if (inputStream is null) throw new Exception("Input wasn't read correctly.");
+
 var inputLines = new StreamReader(inputStream)
     .ReadToEnd()
     .Split("\n")
     .Where(s => !string.IsNullOrWhiteSpace(s));
 
-var games = inputLines.Select(s => s.ParseStringAsDay2Game()).ToList();
+var games = inputLines
+    .Select(s => s.ParseStringAsDay2Game())
+    .ToList();
 
-var possibleGames = games.Where(g => g.Sets.All(set => set.Red <= 12 && set.Green <= 13 && set.Blue <= 14));
+// Part 1
+var sumOfIds = games
+    .Where(g => g.Sets.All(set => set.Red <= 12 && set.Green <= 13 && set.Blue <= 14))
+    .Sum(game => game.Id);
 
-var sumOfIds = possibleGames.Sum(g => g.Id);
-Console.WriteLine("Answer part 1: " + sumOfIds);
+Console.WriteLine($"Answer part 1: {sumOfIds}");
 
-var part2Answer = 0L;
-
-foreach (var g in games)
+// Part 2
+var part2Answer =  games.Select(g =>
 {
     var minimumRed = g.Sets.Max(set => set.Red);
     var minimumGreen = g.Sets.Max(set => set.Green);
     var minimumBlue = g.Sets.Max(set => set.Blue);
-    
-    var product = minimumRed * minimumGreen * minimumBlue;
 
-    part2Answer += product;
-}
+    return minimumRed * minimumGreen * minimumBlue;
+}).Sum();
 
-Console.WriteLine($"Part 2 answer: {part2Answer}");
+Console.WriteLine($"Answer part 2: {part2Answer}");
